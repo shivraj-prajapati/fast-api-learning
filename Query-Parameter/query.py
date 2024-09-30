f8f86eb8390124feb9ab79b00f20967bd9237acb
Shivraj Prajapati <rocky1989sp@gmail.com>
2024-09-30 09:57:28 +0530

    Limit using query parameter

--- Query-Parameter/query.py
+++ Query-Parameter/query.py
@@ -19,6 +19,8 @@ fake_items_db = [
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+#You can request of this url
 if __name__ == "__main__":
     import uvicorn 
     uvicorn.run(app, host="127.0.0.1", port=8080)
